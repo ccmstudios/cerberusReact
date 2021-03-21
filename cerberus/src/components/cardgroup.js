554a4703ce7ce1a092a54f41612e8acc694c019d
N.M. Porterfield <ccmstudiosinc@gmail.com>
2021-03-21 11:55:12 -0400

    props

--- cerberus/src/components/cardgroup.js
+++ cerberus/src/components/cardgroup.js
@@ -5,53 +5,59 @@ import GameOne from '../assets/images/gameone.jpeg'
 import GameTwo from '../assets/images/gametwo.jpeg'
 import GameThree from '../assets/images/gamethree.jpeg'
 
-class Cardgroup extends React.Component{
-    render(){
+const Cardgroup = props =>{
         return(
             <div>
                 <CardGroup>
   <Card bg='secondary' text='light'>
-    <Card.Img variant="top" src={GameOne}/>
+    <Card.Img variant="top" src={GameTwo}/>
     <Card.Body >
-      <Card.Title>Game One</Card.Title>
+      <Card.Title>{props.game}</Card.Title>
       <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
+       Home: {props.home}
+      </Card.Text>
+      <Card.Text>
+       Away: {props.away}
       </Card.Text>
     </Card.Body>
     <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
+      <small className="text">{props.date}</small>
     </Card.Footer>
   </Card>
   <Card bg='secondary' text='light'>
-    <Card.Img variant="top" src={GameTwo} />
-    <Card.Body>
-      <Card.Title>Game Two</Card.Title>
+    <Card.Img variant="top" src={GameTwo}/>
+    <Card.Body >
+      <Card.Title>{props.game}</Card.Title>
       <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
+       <img src={props.homelogo}/>Home: {props.home}
+      </Card.Text>
+      <Card.Text>
+      <img src={props.awaylogo}/>Away: {props.away}
       </Card.Text>
     </Card.Body>
     <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
+      <small className="text">{props.date}</small>
     </Card.Footer>
   </Card>
   <Card bg='secondary' text='light'>
-    <Card.Img variant="top" src={GameThree} />
-    <Card.Body>
-      <Card.Title>Game Three</Card.Title>
+    <Card.Img variant="top" src={GameTwo}/>
+    <Card.Body >
+      <Card.Title>{props.game}</Card.Title>
       <Card.Text>
-        Testing to see how it translates
+       Home: {props.home}
+      </Card.Text>
+      <Card.Text>
+       Away: {props.away}
       </Card.Text>
     </Card.Body>
     <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
+      <small className="text">{props.date}</small>
     </Card.Footer>
   </Card>
+
 </CardGroup>
             </div>
         )
     }
-}
 
 export default Cardgroup;
--- cerberus/src/components/scores.js
+++ cerberus/src/components/scores.js
@@ -17,9 +17,16 @@ function Scores() {
   
     return (
 <div id='scores'>
-    <Carousel activeIndex={index} onSelect={handleSelect}>
+<h1 id='scorestext'>NFL Scores</h1>    
+<br></br>
+<Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-       <Cardgroup/>
+       <Cardgroup game='Atlanta Falcons vs Carolina panthers' 
+        home ='40' 
+        away='39'
+        awaylogo='https://images.search.yahoo.com/search/images;_ylt=Awr9ImAca1dgnwkAFwhXNyoA;_ylu=Y29sbwNncTEEcG9zAzEEdnRpZANBMDYxNV8xBHNlYwNwaXZz?p=atlanta+falcons+logo&fr2=piv-web&fr=mcafee#id=0&iurl=http%3A%2F%2Fflytothegame.com%2Fwp-content%2Fuploads%2F2014%2F07%2FAtlanta-Falcons-team-logo.png&action=click' 
+        homelogo ='https://images.search.yahoo.com/search/images;_ylt=Awr9CJ7galdgaW4AUdRXNyoA;_ylu=Y29sbwNncTEEcG9zAzEEdnRpZANBMDYxNV8xBHNlYwNwaXZz?p=carolina+panthers+logo&fr2=piv-web&fr=mcafee#id=6&iurl=https%3A%2F%2Fimages.homedepot-static.com%2FproductImages%2Fb20fe78a-f543-4a3f-82df-e5f9acd7e363%2Fsvn%2Fblacks-wall-decals-fh89-00496-64_1000.jpg&action=click' 
+        date = 'Dec 10th 2021'/>
 
         <Carousel.Caption>
           {/* <h3>First slide label</h3>
