4708e0ca5175c10c16fa30c5073838600c4193a8
N.M. Porterfield <ccmstudiosinc@gmail.com>
2021-03-28 22:23:59 -0400

    color changes

--- cerberus/src/App.js
+++ cerberus/src/App.js
@@ -24,7 +24,7 @@ function App() {
     <div className="App">
                       <Router>
       <Subheader/>
-                        <Livescores/>
+                        {/* <Livescores/> */}
       <Scores/>
  <Switch>
        
