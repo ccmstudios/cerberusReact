089b2956a12199c513642086cbe7986fae442e84
N.M. Porterfield <ccmstudiosinc@gmail.com>
2021-03-28 22:13:39 -0400

    carousel test

--- cerberus/src/components/scores.js
+++ cerberus/src/components/scores.js
@@ -3,12 +3,37 @@ import React from 'react'
 import { render} from 'react-dom'
 import Cardgroup from './cardgroup'
 import Carousel from 'react-bootstrap/Carousel'
-
+import Game from './game'
 
 // fix the class extends into a Function component This will be where Thomas's API will target.
 
 
 function Scores() {
+  const Logo = "http://loodibee.com/wp-content/uploads/nfl-";
+  const Teams = {
+    ARI: `${Logo}arizona-cardinals-team-logo-2-768x768.png`,
+    ATL: `${Logo}atlanta-falcons-team-logo-2-768x768.png`,
+    BAL: `${Logo}baltimore-ravens-team-logo-2-768x768.png`,
+    BUF: `${Logo}buffalo-bills-team-logo-2-768x768.png`,
+    CAR: `${Logo}carolina-panthers-team-logo-2-768x768.png`,
+    CHI: `${Logo}chicago-bears-team-logo-2-768x768.png`,
+    CIN: `${Logo}cincinnati-bengals-team-logo-768x768.png`,
+    CLE: `${Logo}cleveland-browns-team-logo-2-768x768.png`,
+    DAL: `${Logo}dallas-cowboys-team-logo-2-768x768.png`,
+    DET: `${Logo}detroit-lions-team-logo-2-768x768.png`,
+    GB: `${Logo}green-bay-packers-team-logo-2-768x768.png`,
+    IND: `${Logo}indianapolis-colts-team-logo-2-768x768.png`,
+    KC: `${Logo}kansas-city-chiefs-team-logo-2-768x768.png`,
+    LAR: `${Logo}los-angeles-rams-team-logo-2-768x768.png`,
+    MIA: `${Logo}miami-dolphins-team-logo-2-768x768.png`,
+    MIN: `${Logo}minnesota-vikings-team-logo-2-768x768.png`,
+    NO: `${Logo}new-orleans-saints-team-logo-2-768x768.png`,
+    NYG: `${Logo}new-york-giants-team-logo-2-768x768.png`,
+    NYJ: `${Logo}new-york-jets-team-logo-768x768.png`,
+    PIT: `${Logo}pittsburgh-steelers-team-logo-2-768x768.png`,
+    TB: `${Logo}tampa-bay-buccaneers-team-logo-2-768x768.png`,
+    TEN: `${Logo}tennessee-titans-team-logo-2-768x768.png`,
+  }
     const [index, setIndex] = useState(0);
   
     const handleSelect = (selectedIndex, e) => {
@@ -21,12 +46,15 @@ function Scores() {
 <br></br>
 <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-       <Cardgroup game='Atlanta Falcons vs Carolina panthers' 
-        home ='40' 
-        away='39'
-        awaylogo='https://fbschedules.com/images/logos/nfl/carolina-panthers.png' 
-        homelogo ='https://fbschedules.com/images/logos/nfl/atlanta-falcons.png' 
-        date = 'Dec 10th 2021'/>
+      <Game
+            time="2:32 - 2nd"
+            drive="2nd & 10"
+            yard="MIN 25"
+            channel="NBC"
+            away={{ name: "MIN", score: 24, logo: Teams.MIN }} 
+            home={{ name: "DAL", score: 7, logo: Teams.DAL }}
+            final={false}
+          />
 
         <Carousel.Caption>
           {/* <h3>First slide label</h3>
@@ -34,15 +62,48 @@ function Scores() {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <Cardgroup/>
+      <Game 
+            away={{ name: "DET", score: 13, logo: Teams.DET }} 
+            home={{ name: "CHI", score: 20, logo: Teams.CHI }}
+            final={true}
+          />
 
         <Carousel.Caption>
           {/* <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <Cardgroup/>
+      <Carousel.Item id='testcarousel'>
+      <Game
+            away={{ name: "BAL", score: 49, logo: Teams.BAL }} 
+            home={{ name: "CIN", score: 13, logo: Teams.CIN }}
+            final={true}
+          />
+          <Game
+            away={{ name: "BAL", score: 49, logo: Teams.BAL }} 
+            home={{ name: "CIN", score: 13, logo: Teams.CIN }}
+            final={true}
+          />
+          <Game
+            away={{ name: "BAL", score: 49, logo: Teams.BAL }} 
+            home={{ name: "CIN", score: 13, logo: Teams.CIN }}
+            final={true}
+          />
+          <Game
+            away={{ name: "BAL", score: 49, logo: Teams.BAL }} 
+            home={{ name: "CIN", score: 13, logo: Teams.CIN }}
+            final={true}
+          />
+          <Game
+            away={{ name: "BAL", score: 49, logo: Teams.BAL }} 
+            home={{ name: "CIN", score: 13, logo: Teams.CIN }}
+            final={true}
+          />
+          <Game
+            away={{ name: "BAL", score: 49, logo: Teams.BAL }} 
+            home={{ name: "CIN", score: 13, logo: Teams.CIN }}
+            final={true}
+          />
 
         <Carousel.Caption>
           {/* <h3>Third slide label</h3>
